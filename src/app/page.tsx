9d891294a64387dbff5cb7bf06e41de8428efc99
lanyetc <2216446158@qq.com>
2023-08-26 22:33:34 +0800

    feat: change config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
           </a>
         </div>
       </div>
-
+      <div className={styles.linerTopLeft}></div>
       <div className={styles.center}>
         <Image
           className={styles.logo}
